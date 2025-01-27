93c06886483fd3c5cd4612ec2dbe541a201ad645
= <=>
2025-01-27 18:31:56 +0800

    Auth views with CORS config

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        proxy: {
+          // Proxy API requests to Laravel
+          '/api': 'http://localhost', // Adjust this if needed based on your API base URL
+        },
+        cors: {
+          origin: 'http://laravel-fortify-demo.local', // Allow your Laravel app to access Vite
+          methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+          allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+        },
+      },
 });
